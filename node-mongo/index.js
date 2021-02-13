c75e3b01e2f12b0b9d946d068a43347c967e15bd
Suraj Choubey <srjchoubey2@gmail.com>
2021-02-13 23:42:48 +0530

    Properly committed mongodb example 2 part 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,33 +5,47 @@ const dboper = require('./operations')
 const url = `mongodb://localhost:27017/`;
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client ) => {
-
-	assert.strictEqual(err, null);
+//MongoClient.connect(url, (err, client ) => {
+// this line when added in connect() doesnt give deprecation warning {useNewUrlParser: true, useUnifiedTopology: true}
+MongoClient.connect(url , {useNewUrlParser: true, useUnifiedTopology: true}).then((client) => {
+	//assert.strictEqual(err, null);
 	console.log("Connected successfully to the server");
 	const db = client.db(dbname);
 
-	dboper.insertDocument(db, { name: "Vadonut", description: "Test"}, 'dishes', (result) => {
+	dboper.insertDocument(db, { name: "Vadonut", description: "Test"}, 'dishes')
+	.then((result) => {
+
 		console.log("Insert Document:\n", result.ops);
 
-		dboper.findDocuments(db, 'dishes', (docs) => {
-			console.log('Found Documents:\n', docs);
+		return dboper.findDocuments(db, 'dishes')
+	})
+	.then((docs) => {
+			
+		console.log('Found Documents:\n', docs);
 
-			dboper.updateDocument(db, {name: "Vadonut"}, {description: 'Updated Test'}, 'dishes', (result) => {
-				console.log('Updated Document:\n', result.result);
+		return dboper.updateDocument(db, {name: "Vadonut"}, {description: 'Updated Test'}, 'dishes')
+	})
+	.then((result) => {
+			
+		console.log('Updated Document:\n', result.result);
 
-				dboper.findDocuments(db, 'dishes', (docs)=>{
-					console.log("Found Documents:\n", docs);
+		return dboper.findDocuments(db, 'dishes')
+	})
+	.then((docs)=>{
+					
+		console.log("Found Documents:\n", docs);
 
-					db.dropCollection('dishes', (result) => {
-						console.log("Dropped Collection: ", result);
+		return db.dropCollection('dishes')
+	})
+	.then((result) => {
+		
+		console.log("Dropped Collection: ", result);
 
-						client.close();
-					});
-				});
-			});
-		});
-	});
+		return client.close();
+	})
+	.catch((err) => console.log(err));
+})
+.catch((err) => console.log(err)); // this line of code was added later
 
 
 /*	THIS WAS THE OLD CODE WITH WHICH I HAD ACTUALLY BEGUN YOU CAN REFER TO IT LATER
@@ -58,4 +72,4 @@ MongoClient.connect(url, (err, client ) => {
 	});
 });
 */
-});
+//});
