0033170b20ac0899763dfe54992258d17ff9254e
Suraj Choubey <srjchoubey2@gmail.com>
2021-03-08 23:37:24 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,26 +8,24 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to the server');
 
-    var newDish = Dishes({
-        name: 'Utthapizza',
+    Dishes.create({
+        name: 'Utthapizzaa',
         description: 'test',
-    });
+    })
+    .then((dish) => {
+        console.log(dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes)  => {
+        console.log(dishes);
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)  => {
-            console.log(dishes);
-
-            return Dishes.deleteOne({})
-        })
-        .then(() => {
-            mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+        return Dishes.deleteOne({})
+    })
+    .then(() => {
+        mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 });
